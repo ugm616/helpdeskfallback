517f9e6c34e4033ec91ea6d8d3ed09e2c67107f4
Makktu <johnmcna@gmail.com>
2021-04-18 01:32:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function whenClicked() {
+    alert("You clicked the goshdarned button, yo");
+}
+
+function bufferClick() {
+    theBuffer.textContent = "HELLO BUFFER";
+}
+
+/* function showArgs(arg1, arg2) {
+    console.log("arguments: ", arguments);
+    console.log(Array.from(arguments));
+    let newArr = Array.from(arguments);
+    return newArr;
+}
+let newArr = showArgs("hello", "world");
+
+console.log(`New Array is ${newArr}`); */
+
+const theButton = document.querySelector(".btn-info");
+
+theButton.addEventListener("click", whenClicked);
+
+theBuffer.addEventListener("click", bufferClick);
